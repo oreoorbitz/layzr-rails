dabbd1ea69a82d3cd4a0355d1101f1c20c48747a
oreoorbitz <oreoorbitz@gmail.com>
2019-05-19 17:07:55 -0500

    changed compatibility from lazyr to progressivley

--- lib/layzr-rails.rb
+++ lib/layzr-rails.rb
@@ -35,11 +35,11 @@ ActionView::Helpers::AssetTagHelper.module_eval do
   alias :rails_image_tag :image_tag
 
   def image_tag(*attrs)
-    options, args, layzr_options = extract_options_and_args(*attrs)
+    options, args = extract_options_and_args(*attrs)
     image_html = rails_image_tag(*args)
 
     if options[:lazy]
-      to_lazy_image(image_html, layzr_options)
+      to_lazy_image(image_html)
     else
       image_html
     end
@@ -47,14 +47,10 @@ ActionView::Helpers::AssetTagHelper.module_eval do
 
   private
 
-  def to_lazy_image(image_html, layzr_options)
+  def to_lazy_image(image_html)
     img = Nokogiri::HTML::DocumentFragment.parse(image_html).at_css("img")
 
-    img["data-normal"] = img["src"]
-    unless layzr_options.nil?
-      img['data-retina'] = layzr_options[:retina] unless layzr_options[:retina].blank?
-      img['data-srcset'] = layzr_options[:srcset] unless layzr_options[:srcset].blank?
-    end
+    img["data-progressive"] = img["src"]
     img["src"] = Layzr::Rails.configuration.placeholder
 
     img.to_s.html_safe
@@ -65,13 +61,11 @@ ActionView::Helpers::AssetTagHelper.module_eval do
     if args.size > 1
       options = attrs.last.dup
       args.last.delete(:lazy)
-      layzr_options = args.last.delete(:layzr) || {}
     else
-      layzr_options = {}
       options = {}
     end
 
-    [options, args, layzr_options]
+    [options, args]
   end
 
 end
